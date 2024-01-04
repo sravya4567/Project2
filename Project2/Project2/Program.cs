29d737524727a5e13875e12d30c147b5795a2157
Pranay Gonipalli <pranaygonipalli1303@gmail.com>
2024-01-04 21:38:54 +0530

    Add project files.

---
+++ Project2/Project2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader studentdetails = new StreamReader("c:\\Mphasis\\Mphasis Project2\\Student details.txt");
+                string str = studentdetails.ReadToEnd();
+                Console.WriteLine("student details are:Name-sravya");
+                Console.WriteLine();
+                Console.WriteLine(str);
+                studentdetails.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error" + e.Message);
+            }
+        }        
+    }
+}
